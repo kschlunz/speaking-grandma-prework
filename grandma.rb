112675bcc7abebce25757b195e1849c3d2d15dfd
Kate Schlunz <schlunzk@gmail.com>
2018-02-26 15:47:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   end
 
-  if phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
+  if phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
   end
 
 end

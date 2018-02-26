8cdac16a8de1be4bfe6e907559b745ca590c8590
Kate Schlunz <schlunzk@gmail.com>
2018-02-26 15:46:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
-elsif phrase == phrase.upcase 
+
+  elsif phrase == phrase.upcase
   "NO, NOT SINCE 1938!"
-end
+  end
 
 end
